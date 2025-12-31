4c9d4e7e206a0d3997062be58eec67ad335b838c
David McCullough <dmccullough488@gmail.com>
2025-12-31 07:43:08 -0600

    completed navbar

--- VanLife/index.jsx
+++ VanLife/index.jsx
@@ -1,10 +1,42 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { BrowserRouter, Routes, Route, Link } from 'react-router-dom';
 
+function Navbar() {
+  return (
+    <nav className='nav'>
+      <Link to="/" className='logoLink'>
+        <h1 className='logo'>#VANLIFE</h1>
+      </Link>
+      <div>
+        <Link to="/about" className='navLink'>About</Link>
+        <Link to="/vans" className='navLink'>Vans</Link>
+      </div>
+    </nav>
+  )
+}
+
+function Home() {
+  return (
+    <h1>You got the travel plans, we got the travel vans.</h1>
+  )
+}
+
+function About() {
+  return (
+    <h1>Don’t squeeze in a sedan when you could relax in a van.</h1>
+  )
+}
 
 function App() {
   return (
-    <h1>Start here</h1>
+    <BrowserRouter>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+      </Routes>
+    </BrowserRouter>
   )
 }
 
