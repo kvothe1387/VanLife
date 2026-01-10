109bc186fbda95f012a79e05384e1ce3de040727
David McCullough <dmccullough488@gmail.com>
2026-01-10 05:10:29 -0600

    Moved Header to its own component folder

--- VanLife/index.jsx
+++ VanLife/index.jsx
@@ -5,24 +5,9 @@ import Home from './pages/Home';
 import About from './pages/About';
 import Vans from "./pages/Vans"
 import VanDetail from "./pages/VanDetail"
+import Layout from "./components/Layout"
 
 import "./server"
-
-function Navbar() {
-  return (
-    <header>
-      <nav className='nav'>
-        <Link to="/" className='logo-link'>
-          <h1 className='logo'>#VANLIFE</h1>
-        </Link>
-        <div>
-          <Link to="/about" className='nav-link'>About</Link>
-          <Link to="/vans" className='nav-link'>Vans</Link>
-        </div>
-      </nav>
-    </header>
-  )
-}
 
 function Footer() {
   return (
@@ -35,12 +20,13 @@ function Footer() {
 function App() {
   return (
     <BrowserRouter>
-      <Navbar />
       <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/about" element={<About />} />
-        <Route path="/vans" element={<Vans />} />
-        <Route path="/vans/:id" element={<VanDetail />} />
+        <Route element={<Layout />}>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/vans" element={<Vans />} />
+          <Route path="/vans/:id" element={<VanDetail />} />
+        </Route>
       </Routes>
       <Footer />
     </BrowserRouter>
