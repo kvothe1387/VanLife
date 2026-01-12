409f1782b980da47e3032a1c8b3dc5774679f440
David McCullough <dmccullough488@gmail.com>
2026-01-11 20:55:51 -0600

    Re-org folder structure.
    set up host pages

--- VanLife/index.jsx
+++ VanLife/index.jsx
@@ -3,9 +3,12 @@ import ReactDOM from 'react-dom/client';
 import { BrowserRouter, Routes, Route, Link } from 'react-router-dom';
 import Home from './pages/Home';
 import About from './pages/About';
-import Vans from "./pages/Vans"
-import VanDetail from "./pages/VanDetail"
+import Vans from "./pages/Vans/Vans"
+import VanDetail from "./pages/Vans/VanDetail"
 import Layout from "./components/Layout"
+import Host from "./pages/Host/Host"
+import Income from "./pages/Host/Income"
+import Reviews from './pages/Host/Reviews'
 
 import "./server"
 
@@ -26,6 +29,9 @@ function App() {
           <Route path="/about" element={<About />} />
           <Route path="/vans" element={<Vans />} />
           <Route path="/vans/:id" element={<VanDetail />} />
+          <Route path="/host" element={<Host />} />
+          <Route path="/income" element={<Income />} />
+          <Route path="/reviews" element={<Reviews />} />
         </Route>
       </Routes>
       <Footer />
---
+++ VanLife/pages/Host/Host.jsx
@@ -0,0 +1,8 @@
+import React from "react"
+
+
+export default function Host() {
+  return (
+    <h1>Host Dashboard here</h1>
+  )
+}
