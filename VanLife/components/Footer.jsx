051dce97501e197ccaa7d3cb6f519fba1b24432f
David McCullough <dmccullough488@gmail.com>
2026-01-17 00:34:38 -0600

    added active link styling with navLink

--- VanLife/components/Footer.jsx
+++ VanLife/components/Footer.jsx
@@ -3,7 +3,7 @@ import React from "react"
 export default function Footer() {
   return (
     <footer>
-        &#169; 2022 #VANLIFE
+        &#169; 2026 #VANLIFE
     </footer>
   )
 };
--- VanLife/index.jsx
+++ VanLife/index.jsx
@@ -6,7 +6,7 @@ import About from './pages/About';
 import Vans from "./pages/Vans/Vans"
 import VanDetail from "./pages/Vans/VanDetail"
 import Layout from "./components/Layout"
-import Host from "./pages/Host/Host"
+import Dashboard from "./pages/Host/Dashboard"
 import Income from "./pages/Host/Income"
 import Reviews from './pages/Host/Reviews'
 
@@ -16,14 +16,17 @@ function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route element={<Layout />}>
-          <Route path="/" element={<Home />} />
-          <Route path="/about" element={<About />} />
-          <Route path="/vans" element={<Vans />} />
-          <Route path="/vans/:id" element={<VanDetail />} />
-          <Route path="/host" element={<Host />} />
-          <Route path="/income" element={<Income />} />
-          <Route path="/reviews" element={<Reviews />} />
+        <Route path="/" element={<Layout />}>
+          <Route index element={<Home />} />
+          <Route path="about" element={<About />} />
+          <Route path="vans" element={<Vans />} />
+          <Route path="vans/:id" element={<VanDetail />} />
+
+          <Route path="host" element={<HostLayout />}>
+            <Route index element={<Dashboard />} />
+            <Route path="income" element={<Income />} />
+            <Route path="reviews" element={<Reviews />} />
+          </Route>
         </Route>
       </Routes>
     </BrowserRouter>
