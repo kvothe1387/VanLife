d5d69effc0a6b278e8b354dd6f6c52fc50baa011
David McCullough <dmccullough488@gmail.com>
2026-01-17 05:40:20 -0600

    added host van routes

--- VanLife/components/HostLayout.jsx
+++ VanLife/components/HostLayout.jsx
@@ -12,6 +12,7 @@ export default function HostLayout() {
             <nav className="host-nav">
                 <NavLink to="/host" end style={({isActive}) => isActive ? activeStyle : null }>Dashboard</NavLink>
                 <NavLink to="/host/income" style={({isActive}) => isActive ? activeStyle : null }>Income</NavLink>
+                <NavLink to="/host/vans" style={({isActive}) => isActive ? activeStyle : null }>Vans</NavLink>
                 <NavLink to="/host/reviews" style={({isActive}) => isActive ? activeStyle : null }>Reviews</NavLink>
             </nav>
             <Outlet />
