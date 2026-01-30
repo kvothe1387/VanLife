f53e4b294e01821c43a512f7242804164897b6d2
David McCullough <dmccullough488@gmail.com>
2026-01-30 04:40:48 -0600

    added nested routes for /host/vans/:id/pricing & photos

--- VanLife/index.jsx
+++ VanLife/index.jsx
@@ -12,6 +12,9 @@ import Income from "./pages/Host/Income"
 import HostVans from "./pages/Host/HostVans"
 import HostVanDetail from "./pages/Host/HostVanDetail"
 import Reviews from './pages/Host/Reviews'
+import HostVanInfo from './pages/Host/HostVanInfo';
+import HostVanPhotos from './pages/Host/HostVanPhotos';
+import HostVanPricing from './pages/Host/HostVanPricing';
 
 import "./server"
 
@@ -30,10 +33,13 @@ function App() {
             <Route path="income" element={<Income />} />
             <Route path="reviews" element={<Reviews />} />
             <Route path="vans" element={<HostVans />} />
-            <Route path="vans/:id" element={<HostVanDetail />} />
+            <Route path="vans/:id" element={<HostVanDetail />}>
+              <Route index element={<HostVanInfo />} />
+              <Route path="pricing" element={<HostVanPricing />} />
+              <Route path="photos" element={<HostVanPhotos />} />
+            </Route>
           </Route>
         </Route>
-
       </Routes>
     </BrowserRouter>
   )
--- VanLife/pages/Host/HostVanDetail.jsx
+++ VanLife/pages/Host/HostVanDetail.jsx
@@ -1,5 +1,5 @@
 import React from "react"
-import { useParams, Link } from "react-router-dom"
+import { useParams, Link, Outlet } from "react-router-dom"
 
 export default function HostVanDetail() {
     const { id } = useParams()
@@ -36,6 +36,7 @@ export default function HostVanDetail() {
                         <h4>${currentVan.price}/day</h4>
                     </div>
                 </div>
+                <Outlet />
             </div>
         </section>
     )
