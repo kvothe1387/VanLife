2773880087ba3244b901b7361a821032cd266ffe
David McCullough <dmccullough488@gmail.com>
2026-02-01 20:52:51 -0600

    refactored the search params links to buttons

--- VanLife/pages/Vans/Vans.jsx
+++ VanLife/pages/Vans/Vans.jsx
@@ -38,10 +38,14 @@ export default function Vans() {
     <div className="van-list-container">
       <h1>Explore our van options</h1>
       <div className="van-list-filter-buttons">
-        <Link to="?type=simple" className="van-type simple">Simple</Link>
-        <Link to="?type=luxury" className="van-type luxury">Luxury</Link>
-        <Link to="?type=rugged" className="van-type rugged">Rugged</Link>
-        <Link to="." className="van-type clear-filters">Clear filters</Link>
+        <button onClick={() => setSearchParams({ type: "simple" })}
+          className="van-type simple">Simple</button>
+        <button onClick={() => setSearchParams({ type: "luxury" })}
+          className="van-type luxury">Luxury</button>
+        <button onClick={() => setSearchParams({ type: "rugged" })}
+          className="van-type rugged">Rugged</button>
+        <button onClick={() => setSearchParams({})}
+          className="van-type clear-filters">Clear filter</button>
       </div>
       <div className="van-list">
         {vanElements}
